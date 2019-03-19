25c74df41272ab1779ef3380a537c3679238b435
Eduardo Colina <eacolina@edu.uwaterloo.ca>
2019-03-18 19:41:21 -0500

    Added etherumjs-wallet, web3-utils and rn-nodefiy hack to allow usage of 'crypto' node modules

--- android/app/src/main/java/com/burner_native/MainApplication.java
+++ android/app/src/main/java/com/burner_native/MainApplication.java
@@ -3,6 +3,7 @@ package com.burner_native;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.bitgo.randombytes.RandomBytesPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import org.reactnative.camera.RNCameraPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RandomBytesPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new RNCameraPackage(),
