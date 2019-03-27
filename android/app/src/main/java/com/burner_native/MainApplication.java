9e145433c244283d36a6504795fb8def59908875
Eduardo Colina <eacolina@edu.uwaterloo.ca>
2019-03-27 17:06:20 -0500

    Linked RN-linear-gradient with Android

--- android/app/src/main/java/com/burner_native/MainApplication.java
+++ android/app/src/main/java/com/burner_native/MainApplication.java
@@ -3,6 +3,7 @@ package com.burner_native;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new RandomBytesPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
